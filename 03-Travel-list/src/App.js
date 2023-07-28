bdea3046bc7f8bb59d506a6a3a28f9bd7878dc7e
Abhishek Verma <wrmaabhishek@gmail.com>
2023-07-28 23:48:57 +0530

    created sandbox of 2nd exercise

--- 03-Travel-list/src/App.js
+++ 03-Travel-list/src/App.js
@@ -22,23 +22,25 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity,setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    if(!description) return;
-    const newItem = {description, quantity, packed: false, id: Date.now()};
-    setDescription('');
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
     setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select onChange={(e) => {
-        setQuantity(+e.target.value);
-    
-      }} value={quantity}>
+      <select
+        onChange={(e) => {
+          setQuantity(+e.target.value);
+        }}
+        value={quantity}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
